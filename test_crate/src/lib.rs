e3d22623c57f3ab75a20ca477eb923c42768cc4a
Mike Lubinets <git@mkl.dev>
2020-12-24 21:36:39 +0100

    chore: custom example diagram in example crate

--- test_crate/src/lib.rs
+++ test_crate/src/lib.rs
@@ -3,17 +3,8 @@
 ///
 /// With aquamarine it's possible to embed Mermaid diagrams into your Rust documentation, using the code snippet syntax
 /// ```mermaid
-/// sequenceDiagram
-///     participant Alice
-///     participant Bob
-///     Alice->>John: Hello John, how are you?
-///     loop Healthcheck
-///         John->>John: Fight against hypochondria
-///     end
-///     Note right of John: Rational thoughts <br/>prevail!
-///     John-->>Alice: Great!
-///     John->>Bob: How about you?
-///     Bob-->>John: Jolly good!
+/// graph LR
+///     Source --> aquamarine -- inject mermaid.js --> ms[Modified Source] --> rustdoc --> f[Docs w/ Mermaid!]
 /// ```
 /// The diagram is going to be located in place of the code snippet
 pub fn example() {}
