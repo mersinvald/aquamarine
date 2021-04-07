d2d2d3f317487593f48b85a0d50b4ac296d99193
Le Savon Fou <lesavonfou@gmail.com>
2021-04-07 22:31:33 +0200

    remove fs_extra dependency

--- build.rs
+++ build.rs
@@ -1,21 +1,18 @@
 use std::{ env, fs };
-use fs_extra::{copy_items, dir::CopyOptions};
 
-fn copy_js_folder() {
-    let mut copy_options = CopyOptions::new();
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    copy_options.overwrite = true;
-    copy_options.copy_inside = true;
-
-    let mut docs_dir = out_dir;
+fn copy_mermaid_js() {
+    let mut docs_dir = env::var("OUT_DIR").unwrap();
     docs_dir.push_str("/../../../../doc");
 
     fs::create_dir_all(&docs_dir).unwrap();
-    copy_items(&vec!["./doc/js/mermaid.min.js"], &docs_dir, &copy_options).unwrap();
+
+    let mut mermaid_dst_file = docs_dir;
+    mermaid_dst_file.push_str("/mermaid.min.js");
+
+    fs::copy(&"./doc/js/mermaid.min.js", &mermaid_dst_file).unwrap();
 }
 
 fn main() {
     println!("cargo:rerun-if-changed=./doc/**");
-    copy_js_folder();
+    copy_mermaid_js();
 }
